1162170014fd92e21459a7aa8d7094c8794e94ae
Tamás Háda <tamas.hada@gmail.com>
2024-02-06 11:25:29 +0100

    Change order of dummies and skew repair

--- House_Prices_-_Advanced_Regression_Techniques/main.py
+++ House_Prices_-_Advanced_Regression_Techniques/main.py
@@ -1,13 +1,9 @@
 import pandas as pd
-pd.options.display.max_rows = 100
 import numpy as np
 import matplotlib.pyplot as plt
-import missingno as msno
-from sklearn.impute import SimpleImputer
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-import seaborn as sbs
-# import tensorflow as tf
+%matplotlib inline
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.metrics import mean_squared_error, make_scorer
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
@@ -27,6 +23,8 @@ data = data.drop(['Id'], axis=1)
 data = data[data.GrLivArea <= 4000]
 
 y = data["SalePrice"]
+
+print(f'Is there NaN value: {data.isnull().values.any()}')
 
 data.loc[:, "LotFrontage"] = data.loc[:, "LotFrontage"].fillna(0)
 data.loc[:, "Alley"] = data.loc[:, "Alley"].fillna('None')
@@ -50,6 +48,10 @@ data.loc[:, "PoolQC"] = data.loc[:, "PoolQC"].fillna('None')
 data.loc[:, "Fence"] = data.loc[:, "Fence"].fillna('None')
 data.loc[:, "MiscFeature"] = data.loc[:, "MiscFeature"].fillna('None')
 
+print(f'Is there NaN value: {data.isnull().values.any()}')
+
+print(f'Number or numerical features: {data.select_dtypes("number").columns.size}')
+
 data = data.replace({
     "MSSubClass" : {
       20: 'C20', 30: 'C30', 40: 'C40', 45: 'C45',
@@ -62,10 +64,14 @@ data = data.replace({
       9: "Sep", 10: "Oct", 11: "Nov", 12: "Dec"}
 })
 
+print(f'Number or numerical features: {data.select_dtypes("number").columns.size}')
+
+print(f'Numerical features: {data.select_dtypes("number").columns.size}')
+
 data = data.replace({
     "Alley" : {"None" : 0, "Grvl" : 1, "Pave" : 2},
     "BsmtCond" : {"None" : 0, "Po" : 1, "Fa" : 2, "TA" : 3, "Gd" : 4, "Ex" : 5},
-    "BsmtExposure" : {"None" : 0, "Mn" : 1, "Av": 2, "Gd" : 3},
+    "BsmtExposure" : { "No" : 0, "None" : 0, "Mn" : 1, "Av": 2, "Gd" : 3},
     "BsmtFinType1" : {"None" : 0, "Unf" : 1, "LwQ": 2, "Rec" : 3, "BLQ" : 4, "ALQ" : 5, "GLQ" : 6},
     "BsmtFinType2" : {"None" : 0, "Unf" : 1, "LwQ": 2, "Rec" : 3, "BLQ" : 4, "ALQ" : 5, "GLQ" : 6},
     "BsmtQual" : {"None" : 0, "Po" : 1, "Fa" : 2, "TA": 3, "Gd" : 4, "Ex" : 5},
@@ -85,24 +91,31 @@ data = data.replace({
     "Utilities" : {"ELO" : 1, "NoSeWa" : 2, "NoSewr" : 3, "AllPub" : 4}
 })
 
-cat_feat = data.select_dtypes(include = ["object"]).columns
-num_feat = data.select_dtypes(exclude = ["object"]).columns
+print(f'Numerical features: {data.select_dtypes("number").columns.size}')
+
+cat_feat = data.select_dtypes(exclude = ['number']).columns
+num_feat = data.select_dtypes(include = ['number']).columns
+
 num_feat = num_feat.drop("SalePrice")
+
+print(f'Categorical features: {cat_feat.size} - Numerical features: {num_feat.size}')
 
 data_num = data[num_feat]
 data_cat = data[cat_feat]
-
-data_cat = pd.get_dummies(data_cat)
 
 skewness = data_num.apply(lambda x: skew(x))
 skewed_feat = skewness[abs(skewness) > 0.5].index
 data_num[skewed_feat] = np.log1p(data_num[skewed_feat])
 
+data_cat = pd.get_dummies(data_cat)
+
+print(f'Categorical features: {data.select_dtypes(exclude = ["number"]).columns.size} - Numerical features: {data.select_dtypes(include = ["number"]).columns.size}')
+
 data = pd.concat([data_num, data_cat], axis = 1)
 
 X_train, X_test, y_train, y_test = train_test_split(data, y, test_size = 0.3, random_state = 0)
 
-normalizer = tf.keras.layers.Normalization(axis=-1)
+normalizer = tf.keras.layers.Normalization()
 normalizer.adapt(np.array(X_train))
 
 model = tf.keras.Sequential([
@@ -112,4 +125,12 @@ model = tf.keras.Sequential([
     layers.Dense(1)
 ])
 
-model.compile(loss='mean_absolute_error', optimizer=tf.keras.optimizers.Adam(0.001))
+model.compile(loss='mean_squared_error', optimizer=tf.keras.optimizers.Adam(0.01))
+
+history = model.fit(X_train, y_train, epochs=100)
+
+model.evaluate(X_test, y_test)
+
+print(data.columns)
+print(X_train.shape)
+print(X_train.first)
