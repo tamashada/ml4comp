97576be1717c04adc573d21106c912ffc5fc6af0
Tamás Háda <tamas.hada@gmail.com>
2024-02-07 16:11:25 +0100

    Add training data set and adjust code to it

--- House_Prices_-_Advanced_Regression_Techniques/main.py
+++ House_Prices_-_Advanced_Regression_Techniques/main.py
@@ -7,158 +7,245 @@ from sklearn.metrics import mean_squared_error, make_scorer
 from sklearn.preprocessing import StandardScaler
 from scipy.stats import skew
 
-data = pd.read_csv('train.csv')
+def init(file: str) -> pd.DataFrame:
+  data = pd.read_csv(file)
 
-data.drop_duplicates(['Id'])
-data = data.drop(['Id'], axis=1)
+  data.drop_duplicates(['Id'])
+  data = data.drop(['Id'], axis=1)
 
-# plt.scatter(data.GrLivArea, data.SalePrice, c = "blue", marker = "s")
-# plt.title("Looking for outliers")
-# plt.xlabel("GrLivArea")
-# plt.ylabel("SalePrice")
-# plt.show()
+  # plt.scatter(data.GrLivArea, data.SalePrice, c = "blue", marker = "s")
+  # plt.title("Looking for outliers")
+  # plt.xlabel("GrLivArea")
+  # plt.ylabel("SalePrice")
+  # plt.show()
 
-data = data[data.GrLivArea <= 4000]
+  data = data[data.GrLivArea <= 4000]
 
-data.loc[:, "SalePrice"] = np.log1p(data.loc[:, "SalePrice"])
+  return data
 
-y = data["SalePrice"]
+data_train = init('train.csv')
+data_test = init('test.csv')
 
-print(f'Is there NaN value: {data.isnull().values.any()}')
+y = np.log1p(data_train.loc[:, "SalePrice"])
 
-data.loc[:, "LotFrontage"] = data.loc[:, "LotFrontage"].fillna(0)
-data.loc[:, "Alley"] = data.loc[:, "Alley"].fillna('None')
-data.loc[:, "MasVnrType"] = data.loc[:, "MasVnrType"].fillna('None')
-data.loc[:, "MasVnrArea"] = data.loc[:, "MasVnrArea"].fillna(0)
-data.loc[:, "BsmtQual"] = data.loc[:, "BsmtQual"].fillna('None')
-data.loc[:, "BsmtCond"] = data.loc[:, "BsmtCond"].fillna('None')
-data.loc[:, "BsmtExposure"] = data.loc[:, "BsmtExposure"].fillna('None')
-data.loc[:, "BsmtFinType1"] = data.loc[:, "BsmtFinType1"].fillna('None')
-data.loc[:, "BsmtFinType2"] = data.loc[:, "BsmtFinType2"].fillna('None')
-# Set nan to the most frequent value
-data.loc[:, "Electrical"] = data.loc[:, "Electrical"].fillna(data.Electrical.mode()[0])
-data.loc[:, "FireplaceQu"] = data.loc[:, "FireplaceQu"].fillna('None')
-data.loc[:, "GarageType"] = data.loc[:, "GarageType"].fillna('None')
-# Set nan to the mean value
-data.loc[:, "GarageYrBlt"] = data.loc[:, "GarageYrBlt"].fillna(data.GarageYrBlt.mean())
-data.loc[:, "GarageFinish"] = data.loc[:, "GarageFinish"].fillna('None')
-data.loc[:, "GarageQual"] = data.loc[:, "GarageQual"].fillna('None')
-data.loc[:, "GarageCond"] = data.loc[:, "GarageCond"].fillna('None')
-data.loc[:, "PoolQC"] = data.loc[:, "PoolQC"].fillna('None')
-data.loc[:, "Fence"] = data.loc[:, "Fence"].fillna('None')
-data.loc[:, "MiscFeature"] = data.loc[:, "MiscFeature"].fillna('None')
+data_train = data_train.drop(["SalePrice"], axis=1)
 
-print(f'Is there NaN value: {data.isnull().values.any()}')
+print(data_train.shape)
+print(data_test.shape)
 
-print(f'Number or numerical features: {data.select_dtypes("number").columns.size}')
+def fill_na(data: pd.DataFrame) -> pd.DataFrame:
+  print(f'Is there NaN value: {data.isnull().values.any()}')
 
-data = data.replace({
-    "MSSubClass" : {
-      20: 'C20', 30: 'C30', 40: 'C40', 45: 'C45',
-      50: 'C50', 60: 'C60', 70: 'C70', 75: 'C75',
-      80: 'C80', 85: 'C85', 90: 'C90', 120: 'C120',
-      150: 'C150', 160: 'C160', 180: 'C180', 190: 'C190'},
-    "MoSold" : {
-      1: "Jan", 2: "Feb", 3: "Mar", 4: "Apr",
-      5: "May", 6: "Jun", 7: "Jul", 8: "Aug",
-      9: "Sep", 10: "Oct", 11: "Nov", 12: "Dec"}
-})
+  data.loc[:, "LotFrontage"] = data.loc[:, "LotFrontage"].fillna(0)
+  data.loc[:, "Alley"] = data.loc[:, "Alley"].fillna('None')
+  data.loc[:, "MasVnrType"] = data.loc[:, "MasVnrType"].fillna('None')
+  data.loc[:, "MasVnrArea"] = data.loc[:, "MasVnrArea"].fillna(0)
+  data.loc[:, "BsmtQual"] = data.loc[:, "BsmtQual"].fillna('None')
+  data.loc[:, "BsmtCond"] = data.loc[:, "BsmtCond"].fillna('None')
+  data.loc[:, "BsmtExposure"] = data.loc[:, "BsmtExposure"].fillna('None')
+  data.loc[:, "BsmtFinType1"] = data.loc[:, "BsmtFinType1"].fillna('None')
+  data.loc[:, "BsmtFinType2"] = data.loc[:, "BsmtFinType2"].fillna('None')
+  # Set nan to the most frequent value
+  data.loc[:, "Electrical"] = data.loc[:, "Electrical"].fillna(data.Electrical.mode()[0])
+  data.loc[:, "FireplaceQu"] = data.loc[:, "FireplaceQu"].fillna('None')
+  data.loc[:, "GarageType"] = data.loc[:, "GarageType"].fillna('None')
+  # Set nan to the mean value
+  data.loc[:, "GarageYrBlt"] = data.loc[:, "GarageYrBlt"].fillna(data.GarageYrBlt.mean())
+  data.loc[:, "GarageFinish"] = data.loc[:, "GarageFinish"].fillna('None')
+  data.loc[:, "GarageQual"] = data.loc[:, "GarageQual"].fillna('None')
+  data.loc[:, "GarageCond"] = data.loc[:, "GarageCond"].fillna('None')
+  data.loc[:, "PoolQC"] = data.loc[:, "PoolQC"].fillna('None')
+  data.loc[:, "Fence"] = data.loc[:, "Fence"].fillna('None')
+  data.loc[:, "MiscFeature"] = data.loc[:, "MiscFeature"].fillna('None')
 
-print(f'Number or numerical features: {data.select_dtypes("number").columns.size}')
+  data.loc[:, "MSZoning"] = data.loc[:, "MSZoning"].fillna(data.MSZoning.mode()[0])
+  data.loc[:, "Utilities"] = data.loc[:, "Utilities"].fillna(data.Utilities.mode()[0])
+  data.loc[:, "Exterior1st"] = data.loc[:, "Exterior1st"].fillna(data.Exterior1st.mode()[0])
+  data.loc[:, "Exterior2nd"] = data.loc[:, "Exterior2nd"].fillna(data.Exterior2nd.mode()[0])
+  data.loc[:, "BsmtUnfSF"] = data.loc[:, "BsmtUnfSF"].fillna(0)
+  data.loc[:, "BsmtFinSF1"] = data.loc[:, "BsmtFinSF1"].fillna(0)
+  data.loc[:, "BsmtFinSF2"] = data.loc[:, "BsmtFinSF2"].fillna(0)
+  data.loc[:, "TotalBsmtSF"] = data.loc[:, "TotalBsmtSF"].fillna(0)
+  data.loc[:, "BsmtFullBath"] = data.loc[:, "BsmtFullBath"].fillna(0)
+  data.loc[:, "BsmtHalfBath"] = data.loc[:, "BsmtHalfBath"].fillna(0)
+  data.loc[:, "KitchenQual"] = data.loc[:, "KitchenQual"].fillna(data.KitchenQual.mode()[0])
+  data.loc[:, "Functional"] = data.loc[:, "Functional"].fillna(data.Functional.mode()[0])
+  data.loc[:, "GarageCars"] = data.loc[:, "GarageCars"].fillna(0)
+  data.loc[:, "GarageArea"] = data.loc[:, "GarageArea"].fillna(0)
+  data.loc[:, "SaleType"] = data.loc[:, "SaleType"].fillna(data.SaleType.mode()[0])
 
-print(f'Numerical features: {data.select_dtypes("number").columns.size}')
+  print(f'Is there NaN value: {data.isnull().values.any()}')
 
-data = data.replace({
-    "Alley" : {"None" : 0, "Grvl" : 1, "Pave" : 2},
-    "BsmtCond" : {"None" : 0, "Po" : 1, "Fa" : 2, "TA" : 3, "Gd" : 4, "Ex" : 5},
-    "BsmtExposure" : { "No" : 0, "None" : 0, "Mn" : 1, "Av": 2, "Gd" : 3},
-    "BsmtFinType1" : {"None" : 0, "Unf" : 1, "LwQ": 2, "Rec" : 3, "BLQ" : 4, "ALQ" : 5, "GLQ" : 6},
-    "BsmtFinType2" : {"None" : 0, "Unf" : 1, "LwQ": 2, "Rec" : 3, "BLQ" : 4, "ALQ" : 5, "GLQ" : 6},
-    "BsmtQual" : {"None" : 0, "Po" : 1, "Fa" : 2, "TA": 3, "Gd" : 4, "Ex" : 5},
-    "ExterCond" : {"Po" : 1, "Fa" : 2, "TA": 3, "Gd": 4, "Ex" : 5},
-    "ExterQual" : {"Po" : 1, "Fa" : 2, "TA": 3, "Gd": 4, "Ex" : 5},
-    "FireplaceQu" : {"None" : 0, "Po" : 1, "Fa" : 2, "TA" : 3, "Gd" : 4, "Ex" : 5},
-    "Functional" : {"Sal" : 1, "Sev" : 2, "Maj2" : 3, "Maj1" : 4, "Mod": 5, "Min2" : 6, "Min1" : 7, "Typ" : 8},
-    "GarageCond" : {"None" : 0, "Po" : 1, "Fa" : 2, "TA" : 3, "Gd" : 4, "Ex" : 5},
-    "GarageQual" : {"None" : 0, "Po" : 1, "Fa" : 2, "TA" : 3, "Gd" : 4, "Ex" : 5},
-    "HeatingQC" : {"Po" : 1, "Fa" : 2, "TA" : 3, "Gd" : 4, "Ex" : 5},
-    "KitchenQual" : {"Po" : 1, "Fa" : 2, "TA" : 3, "Gd" : 4, "Ex" : 5},
-    "LandSlope" : {"Sev" : 1, "Mod" : 2, "Gtl" : 3},
-    "LotShape" : {"IR3" : 1, "IR2" : 2, "IR1" : 3, "Reg" : 4},
-    "PavedDrive" : {"N" : 0, "P" : 1, "Y" : 2},
-    "PoolQC" : {"None" : 0, "Fa" : 1, "TA" : 2, "Gd" : 3, "Ex" : 4},
-    "Street" : {"Grvl" : 1, "Pave" : 2},
-    "Utilities" : {"ELO" : 1, "NoSeWa" : 2, "NoSewr" : 3, "AllPub" : 4}
-})
+  return data
 
-print(f'Numerical features: {data.select_dtypes("number").columns.size}')
+data_train = fill_na(data_train)
+data_test = fill_na(data_test)
 
-corr = data.corr()
-corr.sort_values(["SalePrice"], ascending = False, inplace = True)
-# print(corr.SalePrice)
+print(data_train.shape)
+print(data_test.shape)
 
-data["OverallQual-s2"] = data["OverallQual"] ** 2
-data["OverallQual-s3"] = data["OverallQual"] ** 3
-data["OverallQual-Sq"] = np.sqrt(data["OverallQual"])
-data["TotalBsmtSF-2"] = data["TotalBsmtSF"] ** 2
-data["TotalBsmtSF-3"] = data["TotalBsmtSF"] ** 3
-data["TotalBsmtSF-Sq"] = np.sqrt(data["TotalBsmtSF"])
-data["1stFlrSF-2"] = data["1stFlrSF"] ** 2
-data["1stFlrSF-3"] = data["1stFlrSF"] ** 3
-data["1stFlrSF-Sq"] = np.sqrt(data["1stFlrSF"])
-data["GrLivArea-2"] = data["GrLivArea"] ** 2
-data["GrLivArea-3"] = data["GrLivArea"] ** 3
-data["GrLivArea-Sq"] = np.sqrt(data["GrLivArea"])
-data["BsmtQual-s2"] = data["BsmtQual"] ** 2
-data["BsmtQual-s3"] = data["BsmtQual"] ** 3
-data["BsmtQual-Sq"] = np.sqrt(data["BsmtQual"])
-data["ExterQual-2"] = data["ExterQual"] ** 2
-data["ExterQual-3"] = data["ExterQual"] ** 3
-data["ExterQual-Sq"] = np.sqrt(data["ExterQual"])
-data["GarageCars-2"] = data["GarageCars"] ** 2
-data["GarageCars-3"] = data["GarageCars"] ** 3
-data["GarageCars-Sq"] = np.sqrt(data["GarageCars"])
-data["FullBath-2"] = data["FullBath"] ** 2
-data["FullBath-3"] = data["FullBath"] ** 3
-data["FullBath-Sq"] = np.sqrt(data["FullBath"])
-data["KitchenQual-2"] = data["KitchenQual"] ** 2
-data["KitchenQual-3"] = data["KitchenQual"] ** 3
-data["KitchenQual-Sq"] = np.sqrt(data["KitchenQual"])
-data["GarageArea-2"] = data["GarageArea"] ** 2
-data["GarageArea-3"] = data["GarageArea"] ** 3
-data["GarageArea-Sq"] = np.sqrt(data["GarageArea"])
+def num_to_cat(data: pd.DataFrame) -> pd.DataFrame:
+  print(f'Number or numerical features: {data.select_dtypes("number").columns.size}')
 
-cat_feat = data.select_dtypes(exclude = ['number']).columns
-num_feat = data.select_dtypes(include = ['number']).columns
+  data = data.replace({
+      "MSSubClass" : {
+        20: 'C20', 30: 'C30', 40: 'C40', 45: 'C45',
+        50: 'C50', 60: 'C60', 70: 'C70', 75: 'C75',
+        80: 'C80', 85: 'C85', 90: 'C90', 120: 'C120',
+        150: 'C150', 160: 'C160', 180: 'C180', 190: 'C190'},
+      "MoSold" : {
+        1: "Jan", 2: "Feb", 3: "Mar", 4: "Apr",
+        5: "May", 6: "Jun", 7: "Jul", 8: "Aug",
+        9: "Sep", 10: "Oct", 11: "Nov", 12: "Dec"}
+  })
 
-num_feat = num_feat.drop("SalePrice")
+  print(f'Number or numerical features: {data.select_dtypes("number").columns.size}')
 
-print(f'Categorical features: {cat_feat.size} - Numerical features: {num_feat.size}')
+  return data
 
-data_num = data.loc[:, num_feat]
-data_cat = data.loc[:, cat_feat]
+data_train = num_to_cat(data_train)
+data_test = num_to_cat(data_test)
 
-skewness = data_num.apply(lambda x: skew(x))
-skewed_feat = skewness[abs(skewness) > 0.5].index
-data_num.loc[:, skewed_feat] = np.log1p(data_num.loc[:, skewed_feat])
+print(data_train.shape)
+print(data_test.shape)
 
-data_cat = pd.get_dummies(data_cat)
+def cat_to_num(data: pd.DataFrame) -> pd.DataFrame:
+  print(f'Numerical features: {data.select_dtypes("number").columns.size}')
 
-print(f'Categorical features: {data_cat.columns.size} - Numerical features: {data_num.columns.size}')
+  data = data.replace({
+      "Alley" : {"None" : 0, "Grvl" : 1, "Pave" : 2},
+      "BsmtCond" : {"None" : 0, "Po" : 1, "Fa" : 2, "TA" : 3, "Gd" : 4, "Ex" : 5},
+      "BsmtExposure" : { "No" : 0, "None" : 0, "Mn" : 1, "Av": 2, "Gd" : 3},
+      "BsmtFinType1" : {"None" : 0, "Unf" : 1, "LwQ": 2, "Rec" : 3, "BLQ" : 4, "ALQ" : 5, "GLQ" : 6},
+      "BsmtFinType2" : {"None" : 0, "Unf" : 1, "LwQ": 2, "Rec" : 3, "BLQ" : 4, "ALQ" : 5, "GLQ" : 6},
+      "BsmtQual" : {"None" : 0, "Po" : 1, "Fa" : 2, "TA": 3, "Gd" : 4, "Ex" : 5},
+      "ExterCond" : {"Po" : 1, "Fa" : 2, "TA": 3, "Gd": 4, "Ex" : 5},
+      "ExterQual" : {"Po" : 1, "Fa" : 2, "TA": 3, "Gd": 4, "Ex" : 5},
+      "FireplaceQu" : {"None" : 0, "Po" : 1, "Fa" : 2, "TA" : 3, "Gd" : 4, "Ex" : 5},
+      "Functional" : {"Sal" : 1, "Sev" : 2, "Maj2" : 3, "Maj1" : 4, "Mod": 5, "Min2" : 6, "Min1" : 7, "Typ" : 8},
+      "GarageCond" : {"None" : 0, "Po" : 1, "Fa" : 2, "TA" : 3, "Gd" : 4, "Ex" : 5},
+      "GarageQual" : {"None" : 0, "Po" : 1, "Fa" : 2, "TA" : 3, "Gd" : 4, "Ex" : 5},
+      "HeatingQC" : {"Po" : 1, "Fa" : 2, "TA" : 3, "Gd" : 4, "Ex" : 5},
+      "KitchenQual" : {"Po" : 1, "Fa" : 2, "TA" : 3, "Gd" : 4, "Ex" : 5},
+      "LandSlope" : {"Sev" : 1, "Mod" : 2, "Gtl" : 3},
+      "LotShape" : {"IR3" : 1, "IR2" : 2, "IR1" : 3, "Reg" : 4},
+      "PavedDrive" : {"N" : 0, "P" : 1, "Y" : 2},
+      "PoolQC" : {"None" : 0, "Fa" : 1, "TA" : 2, "Gd" : 3, "Ex" : 4},
+      "Street" : {"Grvl" : 1, "Pave" : 2},
+      "Utilities" : {"ELO" : 1, "NoSeWa" : 2, "NoSewr" : 3, "AllPub" : 4}
+  })
 
-data = pd.concat([data_num, data_cat], axis = 1)
+  print(f'Numerical features: {data.select_dtypes("number").columns.size}')
 
-X_train, X_test, y_train, y_test = train_test_split(data, y, test_size = 0.3, random_state = 0)
+  return data
+
+data_train = cat_to_num(data_train)
+data_test = cat_to_num(data_test)
+
+print(data_train.shape)
+print(data_test.shape)
+
+def rank_impor(data: pd.DataFrame) -> pd.DataFrame:
+  # corr = data.corr()
+  # corr.sort_values(["SalePrice"], ascending = False, inplace = True)
+  # print(corr.SalePrice)
+
+  data["OverallQual-s2"] = data["OverallQual"] ** 2
+  data["OverallQual-s3"] = data["OverallQual"] ** 3
+  data["OverallQual-Sq"] = np.sqrt(data["OverallQual"])
+  data["TotalBsmtSF-2"] = data["TotalBsmtSF"] ** 2
+  data["TotalBsmtSF-3"] = data["TotalBsmtSF"] ** 3
+  data["TotalBsmtSF-Sq"] = np.sqrt(data["TotalBsmtSF"])
+  data["1stFlrSF-2"] = data["1stFlrSF"] ** 2
+  data["1stFlrSF-3"] = data["1stFlrSF"] ** 3
+  data["1stFlrSF-Sq"] = np.sqrt(data["1stFlrSF"])
+  data["GrLivArea-2"] = data["GrLivArea"] ** 2
+  data["GrLivArea-3"] = data["GrLivArea"] ** 3
+  data["GrLivArea-Sq"] = np.sqrt(data["GrLivArea"])
+  data["BsmtQual-s2"] = data["BsmtQual"] ** 2
+  data["BsmtQual-s3"] = data["BsmtQual"] ** 3
+  data["BsmtQual-Sq"] = np.sqrt(data["BsmtQual"])
+  data["ExterQual-2"] = data["ExterQual"] ** 2
+  data["ExterQual-3"] = data["ExterQual"] ** 3
+  data["ExterQual-Sq"] = np.sqrt(data["ExterQual"])
+  data["GarageCars-2"] = data["GarageCars"] ** 2
+  data["GarageCars-3"] = data["GarageCars"] ** 3
+  data["GarageCars-Sq"] = np.sqrt(data["GarageCars"])
+  data["FullBath-2"] = data["FullBath"] ** 2
+  data["FullBath-3"] = data["FullBath"] ** 3
+  data["FullBath-Sq"] = np.sqrt(data["FullBath"])
+  data["KitchenQual-2"] = data["KitchenQual"] ** 2
+  data["KitchenQual-3"] = data["KitchenQual"] ** 3
+  data["KitchenQual-Sq"] = np.sqrt(data["KitchenQual"])
+  data["GarageArea-2"] = data["GarageArea"] ** 2
+  data["GarageArea-3"] = data["GarageArea"] ** 3
+  data["GarageArea-Sq"] = np.sqrt(data["GarageArea"])
+
+  return data
+
+data_train = rank_impor(data_train)
+data_test = rank_impor(data_test)
+
+print(data_train.shape)
+print(data_test.shape)
+
+def handle_skew(data: pd.DataFrame) -> pd.DataFrame:
+  num_feat = data.select_dtypes(include = ['number']).columns
+
+  data_num = data.loc[:, num_feat]
+
+  skewness = data_num.apply(lambda x: skew(x))
+  skewed_feat = skewness[abs(skewness) > 0.5].index
+  data_num.loc[:, skewed_feat] = np.log1p(data_num.loc[:, skewed_feat])
+
+  print(f'Numerical features: {data_num.columns.size}')
+
+  return data_num
+
+data_train_num = handle_skew(data_train)
+data_test_num = handle_skew(data_test)
+
+def one_hot_encode(data: pd.DataFrame) -> pd.DataFrame:
+  cat_feat = data.select_dtypes(exclude = ['number']).columns
+
+  data_cat = data.loc[:, cat_feat]
+
+  data_cat = pd.get_dummies(data_cat)
+
+  print(f'Categorical features before align: {data_cat.columns.size}')
+
+  return data_cat
+
+data_train_cat = one_hot_encode(data_train)
+data_test_cat = one_hot_encode(data_test)
+
+final_data_train_cat, final_data_test_cat = data_train_cat.align(data_test_cat, join='inner', axis=1)  # inner join
+
+print(f'Train categorical features after align: {final_data_train_cat.columns.size}')
+print(f'Test categorical features after align: {final_data_test_cat.columns.size}')
+
+data_train = pd.concat([data_train_num, final_data_train_cat], axis = 1)
+data_test = pd.concat([data_test_num, final_data_test_cat], axis = 1)
+
+X_train, X_cv, y_train, y_cv = train_test_split(data_train, y, test_size = 0.3, random_state = 0)
+
+num_feat = data_train.select_dtypes(include = ['number']).columns
 
 scaler = StandardScaler()
 X_train.loc[:, num_feat] = scaler.fit_transform(X_train.loc[:, num_feat])
+X_cv.loc[:, num_feat] = scaler.transform(X_cv.loc[:, num_feat])
+X_test = data_test
 X_test.loc[:, num_feat] = scaler.transform(X_test.loc[:, num_feat])
+
+print(f'Training dataset shape: {X_train.shape}')
+print(f'Cross-validation dataset shape: {X_cv.shape}')
+print(f'Test dataset shape: {X_train.shape}')
 
 _,feat_num = X_train.shape
 
 model = tf.keras.Sequential([
     tf.keras.layers.Input(shape=(feat_num,)),
-    tf.keras.layers.Dense(units=256, activation='relu', kernel_regularizer=tf.keras.regularizers.L2(.001)),
+    tf.keras.layers.Dense(units=512, activation='relu', kernel_regularizer=tf.keras.regularizers.L2(.001)),
     tf.keras.layers.Dense(units=256, activation='relu', kernel_regularizer=tf.keras.regularizers.L2(.001)),
     tf.keras.layers.Dense(units=128, activation='relu', kernel_regularizer=tf.keras.regularizers.L2(.001)),
     tf.keras.layers.Dense(units=64, activation='relu', kernel_regularizer=tf.keras.regularizers.L2(.001)),
@@ -167,11 +254,9 @@ model = tf.keras.Sequential([
 
 model.compile(
     loss=tf.keras.losses.MeanSquaredError(),
-    optimizer=tf.keras.optimizers.Adam(0.0005),
+    optimizer=tf.keras.optimizers.Adam(0.0001),
     metrics=[tf.keras.metrics.MeanSquaredError()])
 
-model.fit(X_train, y_train, batch_size=100, epochs=1000)
+model.fit(X_train, y_train, batch_size=50, epochs=1000)
 
-model.evaluate(X_test, y_test)
-
-data_test = pd.read_csv('test.csv')S
+model.evaluate(X_cv, y_cv)
